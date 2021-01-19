130483aa3745aa68b7b0e4afbcbffae72339d66c
Nick Horton <nhorton04@gmail.com>
2021-01-19 14:04:05 -0800

    login backend complete

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -25,11 +25,55 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc    Register Nu User
+// @route   POST /api/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
+});
+
 // @desc    GEt user profile
 // @route   GET /api/users/profile
 // @access  Private
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.send('Succ');
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
 });
 
-export { authUser, getUserProfile };
+export { authUser, registerUser, getUserProfile };
