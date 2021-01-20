955feefd26c6096a0d613b0c02a93b606e46555d
Nick Horton <nhorton04@gmail.com>
2021-01-19 16:10:01 -0800

    working sign in page

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -7,19 +7,33 @@ import Loader from '../components/Loader';
 import FormContainer from '../components/FormContainer';
 import { login } from '../actions/userActions';
 
-const LoginScreen = ({ location }) => {
+const LoginScreen = ({ history, location }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const dispatch = useDispatch();
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { loading, error, userInfo } = userLogin;
+
   const redirect = location.search ? location.search.split('=')[1] : '/';
+
+  useEffect(() => {
+    if (userInfo) {
+      history.push(redirect);
+    }
+  }, [history, userInfo, redirect]);
+
   const submitHandler = (e) => {
-    e.PreventDefault();
-    // DiSpAtCh LoGIn
+    e.preventDefault();
+    dispatch(login(email, password));
   };
 
   return (
     <FormContainer>
       <h1>Sign In</h1>
+      {error && <Message variant='danger'>{error}</Message>}
+      {loading && <Loader />}
       <Form onSubmit={submitHandler}>
         <Form.Group controlId='email'>
           <Form.Label>Email Address</Form.Label>
@@ -44,7 +58,7 @@ const LoginScreen = ({ location }) => {
           Sign In
         </Button>
 
-        <Row classname='py-3'>
+        <Row className='py-3'>
           <Col>
             New Customer?{' '}
             <Link
